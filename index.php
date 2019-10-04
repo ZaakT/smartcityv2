7a695423123cbc6d57f561ca01d1049820076b22
Florentin Vanaldewereld <f.vanaldewereld@gmail.com>
2019-10-04 16:38:20 +0200

    Initial commit 2

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+$loader = new FilesystemLoader(__DIR__ . '/view/templates');
+$twig = new Environment($loader);
+
+echo $twig->render('index.twig');
