c95f92834cf3b63a2e395c83ee180ab04876e719
Florentin Vanaldewereld <f.vanaldewereld@gmail.com>
2019-10-09 09:52:03 +0200

    1st draft (views)

--- index.php
+++ index.php
@@ -5,7 +5,20 @@ require __DIR__ . '/vendor/autoload.php';
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-$loader = new FilesystemLoader(__DIR__ . '/view/templates');
+$loader = new FilesystemLoader(__DIR__ . '/view');
 $twig = new Environment($loader);
 
-echo $twig->render('homepage.twig');
+
+if(isset($_GET['path'])){
+    $path = $_GET['path'];
+    $is_connected = true;
+    $is_admin = true;
+} else {
+    $path = "/others/login";
+    $is_connected = false;
+    $is_admin = false;
+}
+
+
+echo $twig->render($path.'.twig',array('is_connected'=>$is_connected,'is_admin'=>$is_admin)); 
+//is_admin à récupérer en SQL
