e965e58d740d53b986fcf7731bf9be391c731561
Diego Mejia <diego.mejia@etu.utc.fr>
2020-08-27 12:26:51 +0200

    js deal criteria

---
+++ public/assets/js/custom/deal_criteria/form.js
@@ -0,0 +1,80 @@
+function isSup(idSup, idInf){
+    // Compare the values of the 2 ID ands return true if v1>v2
+    var v1 = $("#"+idSup).val();
+    v1 = v1 ? parseFloat(v1) : -1;
+    var v2 = $("#"+idInf).val();
+    v2 = v2 ? parseFloat(v2) : -1;
+    return v1>=v2;
+}
+
+function isInf(idSup, idInf){
+    // Compare the values of the 2 ID ands return true if v1>v2
+    var v1 = $("#"+idSup).val();
+    v1 = v1 ? parseFloat(v1) : -1;
+    var v2 = $("#"+idInf).val();
+    v2 = v2 ? parseFloat(v2) : -1;
+    return v1<=v2;
+}
+
+
+function checkInput(id){
+    var orderDict = {
+    "payback": false,
+    "societal_payback": false,
+    "roi" : true,  
+    "societal_roi" : true,
+    "npv" : true,
+    "societal_npv" : true,
+    "nqbr" : true,
+    "risks_rating" : false
+    };//true if the target has to be >= than the nogo 
+
+    id = id.getAttribute('id');
+    var idType = id.substr(0, id.lastIndexOf("_"));//payback, societal_payback, ...
+    var idGoal = id.substr(id.lastIndexOf("_")+1);//nogo or target
+    console.log(id, idType, idGoal, orderDict[idType]);
+    var val = $("#"+id).val();
+    console.log(val);
+    val = val ? parseFloat(val) : -1 ;        
+    var temp = String(val).split(".");
+    var opGoal = "nogo";
+    if(idGoal == "nogo"){ opGoal = "target" ;}
+
+    if(val < 0. || (val>100. && ( idType=="roi" || idType=="societal_roi") )){
+        console.log("it's me 0 !");
+        $("#"+id).css("background","salmon");
+        $("#"+id).val("");
+    } else if (temp.length==2 && temp[1].length>3){
+        console.log("it's me 1 !");
+        $("#"+id).css("background","salmon");
+    } else {
+        $("#"+id).css("background","#C3E6CB");
+        
+    }
+    if(orderDict[idType]){//!(((orderDict[idType] && idGoal=="target") || (!orderDict[idType] && idGoal!="target")) && isSup(id, idType.concat("_", opGoal)))
+        if(idGoal == "target" && !isSup(id, idType +"_"+ "nogo")){
+            console.log("it's me 2 !");
+            $("#"+idType +"_"+ "nogo").val($("#"+id).val());
+        }else if(idGoal == "nogo" && !isInf(id, idType +"_"+ "target")){
+            console.log("it's me 3 !");
+            $("#"+idType +"_"+ "target").val($("#"+id).val());
+        }
+    } else {
+        if(idGoal == "target" && !isInf(id, idType +"_"+ "nogo")){
+            console.log("it's me 4 !");
+            console.log("#"+idType +"_"+ "nogo");
+            console.log(isInf(id, idType +"_"+ "nogo"));
+            $("#"+idType +"_"+ "nogo").val($("#"+id).val());
+        }else if(idGoal == "nogo" && !isSup(id, idType +"_"+ "target")){
+            console.log("it's me 5 !");
+            console.log(isInf(id, idType +"_"+ "target"));
+            $("#"+idType +"_"+ "target").val($("#"+id).val());
+        }
+    }  
+
+
+}
+
+$("input").each(function(){
+    checkInput(this);
+});
--- public/assets/js/custom/deal_criteria/from.js
+++
@@ -1,3 +0,0 @@
-function checkInput(id){
-    
-}
