39e8d7b12b44a145e9b1abf4835156ede654e1fa
Diego Mejia <diego.mejia@etu.utc.fr>
2020-09-01 17:42:31 +0200

    debut de non_monetizable

--- controller/output/customer_dashbords.php
+++ controller/output/customer_dashbords.php
@@ -262,7 +262,11 @@ function dashboards_non_monetizable($twig,$is_connected, $projID){
     if($projID!=0){
         if(getProjByID($projID,$user[0])){
             $proj = getProjByID($projID,$user[0]);
-            echo $twig->render('/output/customer_dashboards_steps/non_monetizable.twig',array('is_connected'=>$is_connected,'devises'=>$devises,'selDevSym'=>$selDevSym,'selDevName'=>$selDevName,'is_admin'=>$user[2],'username'=>$user[1],'part'=>"Project",'projID'=>$projID,"selected"=>$proj[1],'projects'=>$list_projects)); 
+            $ucs = getListUCs();
+            $selScope = getListSelScope($projID);
+            echo $twig->render('/output/customer_dashboards_steps/non_monetizable.twig',array('is_connected'=>$is_connected,'devises'=>$devises,'selDevSym'=>$selDevSym,
+            'selDevName'=>$selDevName,'is_admin'=>$user[2],'username'=>$user[1],'part'=>"Project",'projID'=>$projID,"selected"=>$proj[1],'projects'=>$list_projects,
+            'selScope'=>$selScope, 'ucs'=>$ucs)); 
             prereq_dashbords();
         }
     }
