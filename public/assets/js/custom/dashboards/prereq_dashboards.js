124d9dddedf764b714cefe69abd6cf3bb3d0a92c
Diego Mejia <diego.mejia@etu.utc.fr>
2020-09-02 11:10:46 +0200

    correctif bug js dashboards

--- public/assets/js/custom/dashboards/prereq_dashboards.js
+++ public/assets/js/custom/dashboards/prereq_dashboards.js
@@ -1,12 +1,11 @@
 
-function prereq_dashboards(check){
+function prereq_dashboards(){
     console.log("coucou");
     var parts_cond = ["global_dashboard", "cost_benefits","cost_benefits_uc","cost_benefits_all","budget","budget_uc","budget_all","bankability","financing","project_dashboard"];
-    if(check){
-        parts_cond.forEach(part => {
-            $("#"+part).removeClass("disabled");
-        });
-    }
+
+    parts_cond.forEach(part => {
+        $("#"+part).removeClass("disabled");
+    });
+
 }
 
-prereq_dashboards();
