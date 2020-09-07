c65c3d65b574203d947436d9b2c0ccd1c48962ad
Alexandre <alexandrefournier13@gmail.com>
2020-09-07 17:48:52 +0200

    Rename vues supplier pour clarifier

--- controller/input/supplier_bc.php
+++ controller/input/supplier_bc.php
@@ -8,7 +8,7 @@ function supplier_schedule($twig,$is_connected, $projID, $ucID){
     $selDevName = isset($_SESSION['devise_name']) ? $_SESSION['devise_name'] : $devises[1]['name'];
     $selDevSym = isset($_SESSION['devise_symbol']) ? $_SESSION['devise_symbol'] :  $devises[1]['symbol'];
     
-    echo $twig->render('/input/supplier_schedule.twig',array('is_connected'=>$is_connected,'devises'=>$devises,'selDevSym'=>$selDevSym,'selDevName'=>$selDevName,'is_admin'=>$user[3], 'projID'=>$projID, 'ucID'=>$ucID));
+    echo $twig->render('/input/input_project_common_steps/common_schedule.twig',array('is_connected'=>$is_connected,'devises'=>$devises,'selDevSym'=>$selDevSym,'selDevName'=>$selDevName,'is_admin'=>$user[3], 'projID'=>$projID, 'ucID'=>$ucID));
     prereq_ipc(0);
 }
 
@@ -18,7 +18,7 @@ function use_case_schedule($twig,$is_connected, $projID, $ucID){
     $selDevName = isset($_SESSION['devise_name']) ? $_SESSION['devise_name'] : $devises[1]['name'];
     $selDevSym = isset($_SESSION['devise_symbol']) ? $_SESSION['devise_symbol'] :  $devises[1]['symbol'];
     
-    echo $twig->render('/input/use_case_supplier_steps/schedule.twig',array('is_connected'=>$is_connected,'devises'=>$devises,'selDevSym'=>$selDevSym,'selDevName'=>$selDevName,'is_admin'=>$user[3], 'projID'=>$projID, 'ucID'=>$ucID));
+    echo $twig->render('/input/use_case_supplier_steps/project_schedule.twig',array('is_connected'=>$is_connected,'devises'=>$devises,'selDevSym'=>$selDevSym,'selDevName'=>$selDevName,'is_admin'=>$user[3], 'projID'=>$projID, 'ucID'=>$ucID));
 }
 
 function use_case_deployment($twig,$is_connected, $projID, $ucID){
--- public/controller/input/supplier_bc.php
+++ public/controller/input/supplier_bc.php
@@ -17,7 +17,7 @@ function use_case_schedule($twig,$is_connected, $projID, $ucID){
     $selDevName = isset($_SESSION['devise_name']) ? $_SESSION['devise_name'] : $devises[1]['name'];
     $selDevSym = isset($_SESSION['devise_symbol']) ? $_SESSION['devise_symbol'] :  $devises[1]['symbol'];
     
-    echo $twig->render('/input/use_case_supplier_steps/schedule.twig',array('is_connected'=>$is_connected,'devises'=>$devises,'selDevSym'=>$selDevSym,'selDevName'=>$selDevName,'is_admin'=>$user[3], 'projID'=>$projID, 'ucID'=>$ucID));
+    echo $twig->render('/input/use_case_supplier_steps/project_schedule.twig',array('is_connected'=>$is_connected,'devises'=>$devises,'selDevSym'=>$selDevSym,'selDevName'=>$selDevName,'is_admin'=>$user[3], 'projID'=>$projID, 'ucID'=>$ucID));
 }
 
 function use_case_deployment($twig,$is_connected, $projID, $ucID){
