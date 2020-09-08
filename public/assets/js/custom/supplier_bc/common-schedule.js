d8c1851e24a3433d18700222493af449a52061b8
Alexandre <alexandrefournier13@gmail.com>
2020-09-08 14:26:48 +0200

    Améliorations graphiques

--- public/assets/js/custom/supplier_bc/common-schedule.js
+++ public/assets/js/custom/supplier_bc/common-schedule.js
@@ -1,10 +1,19 @@
 function submitForm(formName){
     var validate_form = true
     let inputs = ["#dstart", "#dduration", "#pstart", "#pduration"]
+    var colored_inputs = []
+
     inputs.forEach(i => {
         if($(i).val() == "") {
             $('.alert').addClass("show");
             validate_form = false;
+
+            $(i).addClass("bg-warning")
+            colored_inputs.push(i)
+        } else {
+            if(!colored_inputs.includes(i)) {
+                $(i).removeClass("bg-warning")
+            }
         }
     })
 
