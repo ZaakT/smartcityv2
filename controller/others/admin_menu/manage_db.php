92c7dbf53282cc384c5a59402445216e320dc8e9
Diego Mejia <diego.mejia@etu.utc.fr>
2020-09-25 10:13:08 +0200

    factorisation code

--- controller/others/admin_menu/manage_db.php
+++ controller/others/admin_menu/manage_db.php
@@ -290,7 +290,9 @@ function manage_item($catName,$twig,$is_connected,$isTaken=false){
     $selDevName = isset($_SESSION['devise_name']) ? $_SESSION['devise_name'] : $devises[1]['name'];
     $selDevSym = isset($_SESSION['devise_symbol']) ? $_SESSION['devise_symbol'] :  $devises[1]['symbol'];
     
-    echo $twig->render('/others/admin_menu/manage_db_items/manage_'.$catName.'_item.twig',array('is_connected'=>$is_connected,'devises'=>$devises,'selDevSym'=>$selDevSym,'selDevName'=>$selDevName,'is_admin'=>$user[3],'username'=>$user[1],'listItem'=>$list_item,'catItemName'=>$catName, 'UC'=>$listUC, 'isTaken'=>$isTaken));
+    //echo $twig->render('/others/admin_menu/manage_db_items/manage_'.$catName.'_item.twig',array('is_connected'=>$is_connected,'devises'=>$devises,'selDevSym'=>$selDevSym,'selDevName'=>$selDevName,'is_admin'=>$user[3],'username'=>$user[1],'listItem'=>$list_item,'catItemName'=>$catName, 'UC'=>$listUC, 'isTaken'=>$isTaken));
+    echo $twig->render('/others/admin_menu/manage_db_items/manage_item.twig',array('is_connected'=>$is_connected,'devises'=>$devises,'selDevSym'=>$selDevSym,'selDevName'=>$selDevName,'is_admin'=>$user[3],
+    'username'=>$user[1],'listItem'=>$list_item,'catItemName'=>$catName, 'UC'=>$listUC, 'isTaken'=>$isTaken, "itemName"=>$catName));
     
 }
 
