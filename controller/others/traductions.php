10ab1e9d0825476756b2f3ecea8c71cd176f1d90
Diego Mejia <diego.mejia@etu.utc.fr>
2020-10-14 16:52:47 +0200

    navBar trad ok

--- controller/others/traductions.php
+++ controller/others/traductions.php
@@ -1,6 +1,7 @@
 <?php
 
 $GLOBALS['dicTrad']=[
+    // -------- Home -------- 
     "home_hello" => [
         'en'=>"Hello ", 
         "fr"=>"Bonjour "
@@ -9,10 +10,91 @@ $GLOBALS['dicTrad']=[
         'en'=>"Language :", 
         "fr"=>"Langue :"
     ],
-    "navbar_home" => [
+    "home_urbatisName" => [
+        "en"=>"URBATIS AI"
+    ],
+    // -------- navBar -------- 
+    "navbar_no_devise" => [
+        "en"=>"There is no devise, please contact an administrator"
+    ],
+    "navbar_log_out_conf" => [
+        "en"=>"Logging out confirmation"
+    ],
+    "navbar_log_out_?" => [
+        "en"=>"Do you want to log out ?"
+    ],
+    
+    // -------- Words -------- 
+    "project_design" => [
+        "en"=>"Project Design"
+    ],    
+    "current" => [
+        "en"=>"current"
+    ],    
+    "cost_benefits" => [
+        "en"=>"Cost-Benefits"
+    ],
+    "home" => [
         'en'=>"Home", 
         "fr"=>"Acceuil"
-        ]
+    ],    
+    "input_project" => [
+        "en"=>"Cost-Input Project"
+    ],    
+    "input_use_case" => [
+        "en"=>"Input Use Case"
+    ],    
+    "dashboards" => [
+        "en"=>"Dashboards"
+    ],    
+    "comparaison" => [
+        "en"=>"Comparison"
+    ],    
+    "financing" => [
+        "en"=>"Financing"
+    ],    
+    "funding" => [
+        "en"=>"Funding"
+    ],    
+    "buisness_model" => [
+        "en"=>"Business Model"
+    ],    
+    "project_initialization" => [
+        "en"=>"Project Initialization"
+    ],    
+    "supplier_bc" => [
+        "en"=>"Supplier BC"
+    ],    
+    "input_project_overlay" => [
+        "en"=>"Input Project Overlay"
+    ],    
+    "customer_bc" => [
+        "en"=>"Customer BC"
+    ],    
+    "language" => [
+        "en"=>"Language",
+        "fr"=>"Langue"
+    ],    
+    "french" => [
+        "en"=>"French",
+        "fr"=>"Français"
+    ],    
+    "english" => [
+        "en"=>"English",
+        "fr"=>"Anglais"
+    ],    
+    "currency" => [
+        "en"=>"Currency"
+    ],    
+    "yes" => [
+        "en"=>"Yes",
+        "fr"=>"Oui"
+    ],    
+    "no" => [
+        "en"=>"No",
+        "fr"=>"Non"
+    ]
+
     ];
 
 
